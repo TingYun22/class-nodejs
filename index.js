66f8182bba6459b480d35959140cf0e70a4d5e6d
TingYun <tingyun02@hotmail.com>
2022-02-10 09:02:46 +0800

    json-sale 排序

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 require('dotenv').config();
 
 const express=require('express');
-const app=express();
+const session = require('express-session');
+const MysqlStore=require('express-mysql-session')(session);
+const moment= require('moment-timezone');
 const multer=require('multer');
-const upload=multer({dest: 'tmp_uploads/'});
+// const upload=multer({dest: 'tmp_uploads/'});
+const upload=require(__dirname+'/modules/upload-imgs');
 const fs=require('fs').promises;
+const db = require('./modules/connect-db');
+const sessionStore = new MysqlStore({},db);
+
+const app=express();
 
 app.set('view engine','ejs');
 // app.get('/a.html',(req,res)=>{
@@ -15,11 +22,32 @@ app.use(express.urlencoded({extended:false}));
 // Top-level middleware 放在最前面篩選
 app.use(express.json());
 app.use(express.static('public'));
+app.use(express.static('node_modules/joi'));
+
+
+app.use(session({
+    saveUninitialized: false,
+    resave: false,
+    secret: 'esjrhfsoirtgreshdrtkjtkjoesjtgirehhareahyerdfhdf',
+    store: sessionStore,
+    cookie: {
+        maxAge: 1200000
+    }
+}));
+
+app.use((req, res, next)=>{
+    res.locals.hello = '哈哈';
+    res.locals.toDateString = d => moment(d).format('YYYY-MM-DD');
+    res.locals.toDatetimeString = d => moment(d).format('YYYY-MM-DD HH:mm:ss');
+    next();
+});
+// 自訂的頂層middleware
 
 /* --------後端的路由--------- */
 app.get('/',(req,res)=>{
     // res.send(`<h2>Hello</h2>`);
     res.render('home',{name:'TingYun'});
+    
 });
 app.get('/wow/123',(req,res)=>{
     res.render('home',{name:'TingYun'});
@@ -29,25 +57,37 @@ app.get('/json-sales',(req,res)=>{
     const orderByCol=req.query.orderByCol;
     const orderByRule=req.query.orderByRule;
     const sales=require('./data/sales');
-    // console.log(sales);
-    // res.send(sales[0].name);
-    res.render('json-sales',{sales});
     
-    if(orderByCol==sales.age){
-        if(orderByRule==orderByRule.val()==asc){
-            console.log(asc);
-            res.render(
-                sales.sort(function(a,b){
-                    return a.age-b.age;
-            }));
+    sales.sort(function(a,b){
+        if(orderByRule=='desc'){
+            switch(orderByCol){
+                case 'name':
+                    return a.name > b.name ? -1:1;
+                    
+                case 'age':
+                    return b.age - a.age;
+                    
+                case 'id':
+                    return a.id > b.id ? -1:1;
+                    
+            }
         }else{
-            res.render(
-                sales.sort(function(a,b){
-                return b.age-a.age;
-            }));
+            switch(orderByCol){
+                case 'name':
+                    return a.name > b.name ? 1:-1;
+                    
+                case 'age':
+                    return a.age - b.age;
+                    
+                case 'id':
+                    return a.id > b.id ? 1:-1;
+                    
+            }
         }
-    }
+    });
 
+    console.log(sales);
+    res.render('json-sales', {sales});
     
 });
 app.get('/try-qs',(req,res)=>{
@@ -66,22 +106,70 @@ app.post('/try-post-form', (req, res)=>{
 });
 
 app.post('/try-upload',upload.single('avata'),async (req, res)=>{
-    //    res.json(req.file);
-    const types=['image/jpeg','image/png'];
+       res.json(req.file);
+    
+    /*const types=['image/jpeg','image/png'];
     const f=req.file;
     if(f && f.originalname){
         if(types.includes(f.mimetype)){
             await fs.rename(f.path,__dirname+'/public/img/'+f.originalname);
             return res.redirect('/img/'+f.originalname);
         }else{
-            fs.unlink(f.path,error=>{});
+            // fs.unlink(f.path,error=>{});
+            return  res.send('不支援的檔案類型');
         }
     }
     res.send('bad');
+    */
 })
 
+app.post('/try-uploads',upload.array('photos'),async (req, res)=>{
+    let f=req.files;
+    let ar=[];
+    for(i of f){
+        ar.push(i.filename);
+    }
+    res.json({ar});
+});
+
+app.get('/my-params1/:action/:id?',(req, res)=>{
+    res.json(req.params);
+});
+
+app.get(/^\/m\/09\d{2}-?\d{3}-?\d{3}$/i,(req, res)=>{
+    let u=req.url.split('?')[0];
+    u=u.slice(3);
+    u=u.replace(/-/g,''); // u=u.split('-').join('');
+    // 用空字串取代 -
+    res.json({mobile: u});
+});
 
 
+app.use('/admin2',require(__dirname+'/routes/admin2'));
+app.use('/address-book',require('./routes/address-book'));
+
+app.get('/try-session',(req,res)=>{
+    req.session.my_var = req.session.my_var || 0;
+    req.session.my_var++;
+    res.json(req.session);
+    
+});
+
+app.get('/try-moment',(req,res)=>{
+    const fm = 'YYYY-MM-DD HH:mm:ss';
+    res.json({
+        mo1: moment().format(fm),
+        mo2: moment().tz('Asia/Tokyo').format(fm),
+        mo3: moment(req.session.cookie.expires).format(fm),
+        mo4: moment(req.session.cookie.expires).tz('Asia/Tokyo').format(fm),
+    });
+})
+
+app.get('/try-db',async (req,res)=>{
+    const sql="SELECT * FROM address_book LIMIT 5";
+    const [rs, fields] = await db.query(sql);
+    res.json(rs);
+});
 
 /* --------所有後端路由的後面--------- */
 app.use((req,res)=>{
